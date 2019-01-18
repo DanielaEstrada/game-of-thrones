a1890849bb7abb7cce9211bac727b91d7bbe5868
“DanielaEstrada” <dani.strada@gmail.com>
2019-01-18 19:02:25 -0300

    Inicio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
-/**
- * 
- * Acá va solo el manejo del DOM
- * 
+/*//DOM
+const data = window.EPISODES.episodes;
+
+
+
+//Inicialización de elementos de Materialize
+document.addEventListener('DOMContentLoaded', function () {
+  window.M.AutoInit();
+});
+
+//Imprime tarjetas de todos los personajes al cargar la página
+window.onload = function () {
  */
