5c3d23833a8464e36fcb7fa4898909f78559e4b3
“DanielaEstrada” <dani.strada@gmail.com>
2019-01-19 22:43:40 -0300

    imprimo las cards con foto y nombre de cada personaje

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,31 @@ document.addEventListener('DOMContentLoaded', function () {
 //Imprime tarjetas de todos los personajes al cargar la página
 window.onload = function () {
  */
+ const containerRoot = document.getElementById("root");
+
+const data = (DATA.characters);
+
+
+const showData = (data) =>{
+    let result = "";
+
+   data.forEach(element => {
+
+        result = containerRoot.innerHTML += `
+        <div>
+        <div class="card">
+        <div class="box">
+        <div class="img">
+        <img src=${element.characterImageThumb}>
+        </div>
+        <h2>${element.characterName}<br></h2>        
+      </div>
+    </div>`
+}
+    );
+
+
+return result;
+
+}
+window.onload = showData(data)
